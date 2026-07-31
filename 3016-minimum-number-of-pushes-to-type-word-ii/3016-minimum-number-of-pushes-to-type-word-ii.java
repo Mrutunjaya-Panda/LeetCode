153b44abf150b21c507e243de5489848fc4b6fa1
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-01 00:02:44 +0530

    Time: 77 ms (5.49%), Space: 47.9 MB (87.86%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,37 +1,29 @@
 class Solution {
+
     public int minimumPushes(String word) {
-        // int n = word.length();
-        // //use greedy approach
-        // //8 distinct can be accomodated with each key for minimum
-        // int ans = 0;
-        // for(int i=0;i<n;i++){
-        //     ans += i/8 + 1; //after each of length 8, pushes will increase.
-        // }
+        // Frequency map to store count of each letter
+        Map<Character, Integer> frequencyMap = new HashMap<>();
 
-        // return ans;
-        //but above approach will not work because letters are not always distinct
-        //let's use greedy + sorting based on frequency in descending order.
-        int n = word.length();
-        //use greedy approach
-        //8 distinct can be accomodated with each key for minimum
-        int ans = 0;
-        int[] freq = new int[26];
-        for(char c: word.toCharArray()){
-            freq[c-'a'] = freq[c-'a'] + 1;
-        }
-        //sort frequencies in descending order
-        Arrays.sort(freq);
-        int[] sortedFrequency = new int[26];
-        for(int i=0;i<26;i++){
-            sortedFrequency[i] = freq[25 - i];//to store in descending order
-        }
-        for(int i=0;i<26;i++){
-            //to reduce the push ops. take that elem. which occurs many times
-            // at max 26 distinct chars can be present
-            ans += (sortedFrequency[i])*(i/8 + 1); //after each of length 8, pushes will increase.
+        // Count occurrences of each letter
+        for (char c : word.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
         }
 
-        return ans;
+        // Priority queue to store frequencies in descending order
+        PriorityQueue<Integer> frequencyQueue = new PriorityQueue<>(
+            (a, b) -> b - a
+        );
+        frequencyQueue.addAll(frequencyMap.values());
+
+        int totalPushes = 0;
+        int index = 0;
+
+        // Calculate total number of presses
+        while (!frequencyQueue.isEmpty()) {
+            totalPushes += (index / 8 + 1) * frequencyQueue.poll();
+            index++;
+        }
+
+        return totalPushes;
     }
 }
-//T.C:- O(nlogn)
