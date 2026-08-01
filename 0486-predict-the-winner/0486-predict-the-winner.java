fef805ed5540d71a1f2f25a79a252904ed4199c5
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-02 00:28:51 +0530

    Time: 0 ms (100%), Space: 43 MB (10.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -19,6 +19,24 @@ class Solution {
         return Math.max(take_si,take_ei);
 
     }
+    //let's use memoized approach
+    static int[][] dp;
+    public int helperM(int[] nums, int si, int ei){
+        if(si > ei) return 0;
+        if(si == ei) return nums[si];//p1 can win by choosing that element.
+
+        if(dp[si][ei] != -1) return dp[si][ei];
+        //choices
+        //for next iteration for p1, choose the minimum because maximum would have been
+        //already choosen by p2 and we will be left with minimum to choose .
+        int take_si = nums[si] + Math.min(helperM(nums,si+2,ei),helperM(nums,si+1,ei-1));
+        int take_ei = nums[ei] + Math.min(helperM(nums,si+1,ei-1),helperM(nums,si,ei-2));
+        //now choose the best for p1 between two
+        
+        return dp[si][ei] = Math.max(take_si,take_ei);
+
+    }
+
     public boolean predictTheWinner(int[] nums) {
         //solve for player1 i.e start with it.
         int n = nums.length;
@@ -27,7 +45,12 @@ class Solution {
         for(int num : nums){
            tot += num;
         }
-        int bestP1 = helper(nums,0,n-1);
+
+        dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        int bestP1 = helperM(nums,0,n-1);
         int p2 = tot - bestP1;
         if(bestP1 < p2){
             return false;//p1 cannot win
