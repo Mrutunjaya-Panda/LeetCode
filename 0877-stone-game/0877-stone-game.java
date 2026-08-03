3c8fb5c61898e11902661bcdc6527ce575f5d738
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-03 20:50:15 +0530

    Time: 8 ms (20.22%), Space: 47.7 MB (9.25%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,50 @@
 class Solution {
+    public int helper(int[] piles,int si,int ei){
+        //base case
+        if(si > ei) return 0;
+        if(si == ei) return piles[si];
+
+        //choices
+        int take_si = piles[si] + Math.min(helper(piles,si+2,ei),helper(piles,si+1,ei-1));
+        int take_ei = piles[ei] + Math.min(helper(piles,si+1,ei-1),helper(piles,si,ei-2));
+
+        //take maximum of both
+        return Math.max(take_si,take_ei);
+    }
+
+    //memoized
+    static int[][] dp;
+    public int helperM(int[] piles,int si,int ei){
+        //base case
+        if(si > ei) return 0;
+        if(si == ei) return piles[si];
+
+        if(dp[si][ei] != -1) return dp[si][ei];
+        //choices
+        int take_si = piles[si] + Math.min(helperM(piles,si+2,ei),helperM(piles,si+1,ei-1));
+        int take_ei = piles[ei] + Math.min(helperM(piles,si+1,ei-1),helperM(piles,si,ei-2));
+
+        //take maximum of both
+        return dp[si][ei] = Math.max(take_si,take_ei);
+    }
+
     public boolean stoneGame(int[] piles) {
-        int N = piles.length;
-
-        // dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]].
-        int[][] dp = new int[N+2][N+2];
-        for (int size = 1; size <= N; ++size)
-            for (int i = 0; i + size <= N; ++i) {
-                int j = i + size - 1;
-                int parity = (j + i + N) % 2;  // j - i - N; but +x = -x (mod 2)
-                if (parity == 1)
-                    dp[i+1][j+1] = Math.max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j]);
-                else
-                    dp[i+1][j+1] = Math.min(-piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j]);
-            }
-
-        return dp[1][N] > 0;
+        int n = piles.length;
+        //doing from alice perspective
+        dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        int tot = 0;
+        for(int pile : piles){
+            tot += pile;
+        }
+        //find the alices best o/p
+        int bestAlice = helperM(piles,0,n-1);
+        if(tot - bestAlice > bestAlice){
+            //tot - bestAlice = bob
+            return false;
+        }
+        return true;//Alice can win
     }
 }
