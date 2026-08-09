a9500d9dc2727b439de8611ebbec8ac3dfe909d8
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-09 22:33:16 +0530

    Time: 48 ms (13.16%), Space: 150.6 MB (5.26%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,31 +1,84 @@
-class Solution {
+import java.util.*;
 
+class Solution {
     public int[] validSequence(String word1, String word2) {
-        int n = word1.length(),
-            m = word2.length();
-        int[] last = new int[m];
-        Arrays.fill(last, -1);
+
+        int n = word1.length();
+        int m = word2.length();
+
+        if (n < m) return new int[0];
+
+        int[] rightHandMatchLength = new int[n + 1];
+
+        int match = 0;
+        int i = n - 1;
         int j = m - 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (j >= 0 && word1.charAt(i) == word2.charAt(j)) {
-                last[j] = i;
-                j -= 1;
+        int k = n - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (word1.charAt(i) == word2.charAt(j)) {
+                match++;
+                rightHandMatchLength[k] = match;
+                i--;
+                j--;
+                k--;
+            } else {
+                rightHandMatchLength[k] = match;
+                i--;
+                k--;
             }
         }
-        int[] res = new int[m];
-        int skip = 0;
+
+        while (k >= 0) {
+            rightHandMatchLength[k] = match;
+            k--;
+        }
+
+        rightHandMatchLength[n] = 0;
+
+        i = 0;
         j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (j == m) break;
-            if (
-                word1.charAt(i) == word2.charAt(j) ||
-                (skip == 0 && (j == m - 1 || i < last[j + 1]))
-            ) {
-                skip += word1.charAt(i) != word2.charAt(j) ? 1 : 0;
-                res[j] = i;
-                j += 1;
+
+        boolean canChange = false;
+
+        List<Integer> res = new ArrayList<>();
+
+        while (i < n && j < m) {
+
+            if (word1.charAt(i) == word2.charAt(j)) {
+
+                res.add(i);
+                i++;
+                j++;
+            }
+            else if (!canChange &&
+                    rightHandMatchLength[i + 1] >= (m - j - 1)) {
+
+                res.add(i);
+
+                canChange = true;
+
+                i++;
+                j++;
+            }
+            else {
+                i++;
             }
         }
-        return j == m ? res : new int[0];
+
+        if (j != m || res.size() != m) {
+            return new int[0];
+        }
+
+        int[] ans = new int[m];
+
+        for (int idx = 0; idx < m; idx++) {
+            ans[idx] = res.get(idx);
+        }
+
+        return ans;
     }
 }
+
+//T.C:- O(n+m)
