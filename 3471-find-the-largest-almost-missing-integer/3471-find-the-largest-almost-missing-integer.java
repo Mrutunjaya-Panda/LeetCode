dd2847ad50d9083cb4c0128856ff07987162e31b
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-19 01:04:32 +0530

    Time: 1 ms (99.3%), Space: 44.4 MB (94.39%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,33 +1,45 @@
 class Solution {
-
+    //It is simply dividing into 3 cases
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
-        if (n == k) {
+        if(k == n){
             int res = nums[0];
-            for (int x : nums) {
-                res = Math.max(res, x);
+            //only one subarray possible so return the largest elem.
+            for(int x:nums){
+                res = Math.max(res,x);
             }
             return res;
         }
-        int[] count = new int[51];
-        for (int x : nums) {
-            count[x]++;
+
+        int cnt[] = new int[51];
+        for(int num:nums){
+            cnt[num] = cnt[num] + 1;
         }
-        if (k == 1) {
-            for (int i = 50; i >= 0; --i) {
-                if (count[i] == 1) {
+        if(k == 1){
+            //n possible subarrays
+            //we need to return the largest one and which appears only in one subarray
+            for(int i=50;i>=0;i--){
+                if(cnt[i] == 1){
                     return i;
                 }
             }
             return -1;
         }
+
+        //now for case 1 < k < n
+        //the middle elements will atleast appear twice since it can occur in atleast
+        //two sliding windows
+        //So possible candidates in only first and the last elem.
+
         int res = -1;
-        if (count[nums[0]] == 1) {
-            res = Math.max(res, nums[0]);
+        if(cnt[nums[0]] == 1){
+            res = Math.max(res,nums[0]);
         }
-        if (count[nums[n - 1]] == 1) {
-            res = Math.max(res, nums[n - 1]);
+
+        if(cnt[nums[n-1]] == 1){
+            res = Math.max(res,nums[n-1]);
         }
+
         return res;
     }
 }
