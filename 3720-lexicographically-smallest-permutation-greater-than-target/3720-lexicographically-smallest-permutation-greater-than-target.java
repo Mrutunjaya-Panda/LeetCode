edfc30b8b5a199b192d39b5871b15de3752b087d
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-08-28 00:23:29 +0530

    Time: 3 ms (58.06%), Space: 44.6 MB (83.87%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -1,77 +1,60 @@
+//recursive solution
 class Solution {
+    String result = "";
+    //greater to check if we have got our next smallest greater.
+    //i for traversal in target
+    public boolean solve(StringBuilder curr, int[] cnt, String target, int i, boolean greater){
+        if(i == target.length()){
+            if(greater){
+                result = curr.toString();
+                return true;
+            }
+            return false;
+        }
+
+        //now start forming the string
+        //for each place, choose the correct char.
+        //we are starting from 'a' because we need smallest lexicographic.
+        for(char ch = 'a';ch <= 'z';ch++){
+            if(cnt[ch - 'a'] == 0){
+                //means not present in s, just continue
+                continue;
+            }
+
+            if(!greater && ch < target.charAt(i)){
+                //since curr tar idx > than curr char.
+                continue;
+            }
+
+            //do
+            curr.append(ch);
+            cnt[ch - 'a']--;
+
+            //recursive call for next indices
+            //explore
+            boolean isGreater = greater || (ch > target.charAt(i));
+            if(solve(curr,cnt,target,i+1,isGreater)){
+                return true;
+            }
+
+            //undo, the path taken didn't conclude
+            curr.deleteCharAt(curr.length() - 1);
+            cnt[ch - 'a']++;
+        }
+
+        return false;
+    }
     public String lexGreaterPermutation(String s, String target) {
-        //first cnt chars present in s.
+        
+        int cnt[] = new int[26];
         int n = s.length();
-
-        int[] cnt = new int[26];
         for(int i=0;i<n;i++){
-            char c = s.charAt(i);
-            int val = c - 'a';
-            cnt[val]++;
+            cnt[s.charAt(i) - 'a']++;
         }
 
-        //traverse over target.
-        StringBuilder res = new StringBuilder();
-        int m = target.length();
-        for(int i=0;i<m;i++){
-            //case 1:- similar elements add orderwise
-            char tarChar = target.charAt(i);
-            int tar = tarChar - 'a';
-            if(cnt[tar] > 0){
-                //means we have same char in string s.
-                cnt[tar] --;
-                //now check for next i+1 chars in target, if forming greater.
-                if(canFormGreater(cnt,target,i+1)){
-                    //then append current char
-                    res.append(tarChar);
-                    //check for other possibilities first
-                    continue;
-                }
-                //cannot form, backtrack/restore
-                cnt[tar]++;
-            }
-
-            //case 2:- fill res current target Idx with just greater char from s.
-            for(int j=tar+1;j<26;j++){
-                if(cnt[j] > 0){
-                    cnt[j] --;
-                    res.append((char)('a'+j));
-                    //append remaining string from s as first greater elem is ad
-                    res.append(getMinString(cnt));
-                    return res.toString();
-                }
-            }
-            return "";
-        }
-        return "";
-    }
-
-    // Check if the remaining characters can form a string greater than the suffix.
-    private boolean canFormGreater(int[] cnt, String target, int start) {
-        String maxStr = getMaxString(cnt);
-        String suffix = target.substring(start);
-        return maxStr.compareTo(suffix) > 0;
-    }
-
-    // Get the maximum lexicographical string (in descending order)
-    private String getMaxString(int[] cnt) {
-        StringBuilder res = new StringBuilder();
-        for (int i = 25; i >= 0; i--) {
-            if (cnt[i] > 0) {
-                res.append(String.valueOf((char) ('a' + i)).repeat(cnt[i]));
-            }
-        }
-        return res.toString();
-    }
-
-    // Get the lexicographically smallest string (in ascending order)
-    private String getMinString(int[] cnt) {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            if (cnt[i] > 0) {
-                res.append(String.valueOf((char) ('a' + i)).repeat(cnt[i]));
-            }
-        }
-        return res.toString();
+        //recursive call
+        StringBuilder curr = new StringBuilder();//to form the current str
+        solve(curr,cnt,target,0,false);
+        return result;
     }
 }
