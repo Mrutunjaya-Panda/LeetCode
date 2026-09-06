0d1cd2da5b7770202df993208077bce57409370c
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-09-06 17:18:37 +0530

    Time: 21 ms (33.78%), Space: 66.5 MB (6.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,29 +1,32 @@
-//Approach-1 (Recursion + MEmoization)
+//Approach-2 (Bottom UP DP)
 //T.C : O(m*n)
 //S.C : O(m*n)
 class Solution {
-    int[][] dp = new int[1001][1001];
-    int solve(String s, String t, int m, int n) {
-        if(n == 0)
-            return dp[m][n] = 1;
-        if(m == 0)
-            return dp[m][n] = 0;
-        
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        if(s.charAt(m-1) == t.charAt(n-1))
-            return dp[m][n] = solve(s, t, m-1, n) + solve(s, t, m-1, n-1);
-        else
-            return dp[m][n] = solve(s, t, m-1, n);
-    }
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        if(m < n)
-            return 0;
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(s, t, m, n);
+
+        
+        long[][] dp = new long[m+1][n+1];
+        
+        for(int row = 0; row<m+1; row++) {
+            dp[row][0] = 1;
+        }
+        
+        for(int col = 1; col<n+1; col++) {
+            dp[0][col] = 0;
+        }
+        
+        for(int i = 1; i<m+1; i++) {
+            for(int j = 1; j<n+1; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        
+        return (int) dp[m][n];
     }
 }
