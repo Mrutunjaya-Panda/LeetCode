21a31c79762bd869a78e565cc13295561d0bd8e5
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-09-08 02:09:54 +0530

    Time: 4 ms (68.73%), Space: 44.4 MB (20.28%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,18 +1,35 @@
 class Solution {
-    int MOD = 1000000007;
+    int M = 1000000007;
+    int[] dp = new int[2001];
+    int[] prev; //prev[n] = last time when we saw this nth character (1-based indexing)
 
+    //memoize
+    public int solve(int n){
+        if(n == 0){
+            return 1;//empty subsequence
+        }
+        if(dp[n] != -1) return dp[n];
+
+        int total = (2*solve(n-1)%M);
+        //remove duplicates
+        if(prev[n] != 0){
+            int duplicates = solve(prev[n] - 1);
+            total = (total - duplicates+M)%M;
+        }
+        return dp[n] = total;
+    }
     public int distinctSubseqII(String s) {
-        int tot = 0;
-        int[] dp = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            int c = s.charAt(i) - 97;
-            int add = (tot - dp[c] + MOD) % MOD;
-
-            dp[c] = 1 + tot;
-            tot = (dp[c] + add) % MOD;
+        int n = s.length();
+        Arrays.fill(dp,-1);
+        prev = new int[n+1];//1-based indexing
+        int lastSeen[] = new int[26];
+        //form prev array
+        for(int i=1;i<=n;i++){
+            int charIdx = s.charAt(i-1) - 'a';
+            prev[i] = lastSeen[charIdx];
+            lastSeen[charIdx] = i;
         }
 
-        return tot;
+        return (solve(n) - 1+M)%M;//removing empty subsequence
     }
 }
