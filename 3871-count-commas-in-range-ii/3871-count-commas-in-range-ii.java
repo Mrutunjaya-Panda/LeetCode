e6ea8eff02e5edef680c71a88bc354c205006815
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-09-09 18:21:52 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -3,15 +3,21 @@ class Solution {
         long cnt = 0;
         long lower = 1000;
         long comma = 1;
+        //simplified counting simulation
         while(lower <= n){
-            long upper = lower*1000 - 1;
-            //safety check
-            if(upper > n) upper = n;
+            // long upper = lower*1000 - 1;
+            // //safety check
+            // if(upper > n) upper = n;
 
-            cnt += comma*(upper - lower + 1);
-            lower = upper+1;
-            comma += 1;
+            // cnt += comma*(upper - lower + 1);
+            // lower = upper+1;
+            // comma += 1;
+            cnt += (n-lower+1);
+            lower = lower*1000;
         }
         return cnt;
     }
 }
+
+//T.C : O(log1000(n))
+//S.C : O(1)
