f012c20ac05f75e951a7d23709e7d3810bae1bd5
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-09-15 17:04:09 +0530

    Time: 2166 ms (5.03%), Space: 183.1 MB (9.55%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isPalindrome(String s,int st,int e){
+        if(st>e) return false;
+        while(st<=e){
+            if(s.charAt(st) == s.charAt(e)){
+                st++;
+                e--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    // public boolean isPalindrome(String s, int i, int j) {
+    //     while (i < j) {
+    //         if (s.charAt(i++) != s.charAt(j--)) return false;
+    //     }
+    //     return true;
+    // }
+    //let's memoize this.
+    int[][] dp;
+    public int solve(String s,int k,int i,int j){
+        //base case, out-of-bound.
+        if(j >= s.length() || i >= s.length()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        if(isPalindrome(s,i,j)){
+            //possibilities
+            //it is palindrome & take, try on next non-overlapping substring.
+            int take = 1 + solve(s,k,j+1,j+k);
+            //first extend current substring as len can be >= k.
+            int grow = solve(s,k,i,j+1);
+            //else just slide, with the smae length i.e =k
+            int slide = solve(s,k,i+1,j+1);
+
+            return dp[i][j] = Math.max(take, Math.max(grow,slide));
+        }
+        //else if the current is not a palindrome, we basically have two options.
+        int grow = solve(s,k,i,j+1);
+        //else just slide, with the smae length i.e =k
+        int slide = solve(s,k,i+1,j+1);
+        return dp[i][j] = Math.max(grow,slide);
+    }
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+        if(k == 1) return n;
+        dp = new int[n+1][n+1];
+        for(int[] row: dp){
+            Arrays.fill(row,-1);
+        }
+
+        return solve(s,k,0,k-1);
+    }
+}
+//T.C : O(n^3)
+//S.C : O(n^2)
