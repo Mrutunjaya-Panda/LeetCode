91fe509fd9539fe62049ddb8a666e28a3899f548
Mrutunjaya Panda <151531154+Mrutunjaya-Panda@users.noreply.github.com>
2026-09-25 11:56:55 +0530

    Time: 83 ms (5.09%), Space: 166.1 MB (5.01%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,55 +1,27 @@
-class Solution {
-    //it just reduces down to finding longest subarray with sum = total_sum - x length finding.
-    //then subtracting from whole length.
-    //Prefix_sum+hashMap.
-    public int solve(int[] nums,int tar){
-        int n = nums.length;
-        Map<Integer,Integer> mp = new HashMap<>();//prefix_sum,index.
-        mp.put(0,-1);
-
-        int sum =0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            mp.put(sum,i);
-        }
-
-        sum=0;
-        int longestLen = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-            int remSumReq = sum-tar;
-
-            if(mp.containsKey(remSumReq)){
-                //means we found a subarray with target = tar.
-                //whhose length
-                longestLen = Math.max(longestLen, i-mp.get(remSumReq));
-            }
-        }
-        return longestLen;
-    }
-    public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        int tot_sum=0;
-        for(int num:nums){
-            tot_sum += num;
-        }
-
-        //now our target = tot_sum -x.
-        int tar = tot_sum-x;
-        if(tar < 0){
-            return -1;
-        }
-        //find longest subarray length with sum = tar.
-        int len = solve(nums,tar);
-
-        if(len == Integer.MIN_VALUE){
-            return -1;
-        }
-        //since we need the other side of len or min. ops.
-        return n-len;
-    }
-}
-
 //Using longest subarray Sum logic
 //T.C : O(n)
 //S.C : O(n)
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length;
+        int sum = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, -1);
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            mp.put(sum, i);
+        }
+        if (sum < x)
+            return -1;
+        int restSum = sum - x;
+        int longest = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (mp.containsKey(sum - restSum)) {
+                longest = Math.max(longest, i - mp.get(sum - restSum));
+            }
+        }
+        return longest == Integer.MIN_VALUE ? -1 : n - longest;
+    }
+}
